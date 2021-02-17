46b0dae75c16a9722ae00bb50965c4e0602df877
Eric Wang <ericwang@Erics-MacBook-Pro.local>
2021-02-17 21:33:53 +0800

    added start script

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get("/", (req, res) => {
 });
 const port = process.env.port || 3000;
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log("listening on port", port);
 });
